464ab437b41f800db7f5704e69675addbf1f0829
LauraCavo <laura.cavo@outlook.com>
2023-08-10 16:16:29 -0300

    se arregló ruta productos,se modificó header.ejs y se agregó links partials en las views

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,24 +1,31 @@
-const path = require('path');
-const arrLibros = require('../dataBase/libros.json');
-
+const path = require("path");
+const arrLibros = require("../dataBase/libros.json");
 
 const productsController = {
-    getAll: (req,res)=>{
-        console.log(arrLibros);
-        //  const { results } = arrLibros;
+  getAll: (req, res) => {
+    res.render("product", arrLibros);
+    //console.log(arrLibros);
+    //  const { results } = arrLibros;
 
-        //  console.log(results)
+    //  console.log(results)
 
-        // res.render('product', { libros: results });    
-    },   
-    byId: (req,res)=>{
-        const {id} = req.params;
-    
-        const findLibro = arrLibros.find((libro) =>  libro.id == id);
+    //  res.render("product", { libros: results });
+  },
+  byId: (req, res) => {
+    // const id = req.params.id;
+    const { id } = req.params;
+    const { results } = arrLibros;
 
-        res.render('productDetail', { libroSelect: findLibro })
-    } 
-};    
+    const product = results.find((prod) => prod.id === id);
 
+    res.render("product", { product });
+
+    /*     const { id } = req.params;
+
+    const findLibro = arrLibros.find((libro) => libro.id == id);
+
+    res.render("productDetail", { libroSelect: findLibro }); */
+  },
+};
 
 module.exports = productsController;
