3fe174672496420eaf8f770ffa6974ce1907a21b
LauraCavo <laura.cavo@outlook.com>
2023-08-26 08:40:12 -0300

    modificaciones routes

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,12 +1,10 @@
-const express = require ('express');
+const express = require("express");
 const route = express.Router();
-const mainController = require ('../controllers/mainController')
+const mainController = require("../controllers/mainController");
 
-route.get('/', mainController.home);
-route.get('/register', mainController.register);
-route.get('/login', mainController.login);
-route.get('/administrador' , mainController.admin);
+route.get("/", mainController.home);
+route.get("/register", mainController.register);
+route.get("/login", mainController.login);
+route.get("/administrador", mainController.admin);
 
-
-
-module.exports=route;
+module.exports = route;
