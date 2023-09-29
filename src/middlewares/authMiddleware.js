d4afffd8e74968232703d7ffad861dd48b428167
LeandroOrtiz91 <leandroortiz.sn@gmail.com>
2023-09-29 20:58:33 -0300

    authMiddleware.js
    Arme este Middleware para redirigir a login y proteger el carrito

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,17 @@
+
+
+
+const authMiddleware = (req, res, next) =>{
+    if (req.session.userLogged) {
+        next()
+
+    }
+    else {
+        res.redirect('../users/login')
+        
+    }
+    
+//     next()
+ }
+
+module.exports = authMiddleware;
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const route = express.Router();
 const mainController = require("../controllers/mainController");
-
+const authMiddleware = require("../middlewares/authMiddleware");
 
 route.get("/", mainController.home);
+
 
 module.exports = route;
