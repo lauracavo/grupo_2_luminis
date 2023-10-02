8592191f327a231566530374db36914e1555e09c
TomasD1998 <tomidistefano@hotmail.com>
2023-10-02 13:09:40 -0300

    Más traducciones. Faltan traducir vistas y estilos

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,12 +3,12 @@ const route = express.Router();
 const adminController = require("../controllers/adminController");
 /* const multer = require("multer"); */
 
-/* localhost:2020/administrador */
+/* localhost:2020/admin */
 route.get("/", adminController.getAll);
 route.get("/formCreate", adminController.create);
 route.post("/store", adminController.store); // Cambiado a ruta "/store"
-route.get('/editar/:id', adminController.edit); // renderiza el form edit
-route.put('/editar/:id', adminController.editProduct); // Guarda los cambios realizados
+route.get('/edit/:id', adminController.edit); // renderiza el form edit
+route.put('/edit/:id', adminController.editProduct); // Guarda los cambios realizados
 route.delete ("/delete/:id", adminController.delete);
 
 module.exports = route;
