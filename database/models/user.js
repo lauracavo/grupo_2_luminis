cc9c789a80c46d18c3d6251e04b50ec02775bce1
LeandroOrtiz91 <leandroortiz.sn@gmail.com>
2023-10-20 15:18:21 -0300

    Update models user

--- database/models/user.js
+++ database/models/user.js
@@ -1,69 +1,84 @@
-const {Sequelize, DataTypes} = require ('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 // const sequelize = new Sequelize
- 
 
 
-    //Aca definimos el modelo
-    module.exports = (sequelize, DataTypes) => {
-        const User = sequelize.define('User', {
-    // define los campos del modelo y tipos de datos
-    idUser: {
-        type: DataTypes.INTEGER,        
-        autoIncrement: true,
-        primaryKey: true
+
+//Aca definimos el modelo
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        // define los campos del modelo y tipos de datos
+        idUser: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        fullName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+
+        // lastName:{
+        //     type:DataTypes.STRING,
+        //     allowNull: false
+        // },
+
+
+        // dni:{
+        //   type:DataTypes.STRING,
+        //   allowNull: false
+        // },
+
+
+        // userName:{
+        //     type:DataTypes.STRING,
+        //     allowNull: false
+        // },
+
+
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+
+
+        // cellPhone:{
+        //     type : DataTypes.DOUBLE,
+        //     allowNull : true
+        // },
+        // address:{
+        //     type:DataTypes.INTEGER,
+        //     allowNull: true
+        // },
+        // city:{
+        //     type:DataTypes.INTEGER,
+        //     allowNull: true
+        // },
+        // province:{
+        //     type:DataTypes.INTEGER,
+        //     allowNull: true
+        // },
+        // cp:{
+        //     type:DataTypes.INTEGER,
+        //     allowNull: true
+        // },
+
+
+        rol: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
     },
-    name:{
-        type:DataTypes.STRING,
-        allowNull:false
-    },
-    lastName:{
-        type:DataTypes.STRING,
-        allowNull: false
-    },
-    dni:{
-      type:DataTypes.STRING,
-      allowNull: false
-    },
-    userName:{
-        type:DataTypes.STRING,
-        allowNull: false
-    },
-    password:{
-        type:DataTypes.STRING,
-        allowNull:false
-    },
-    email:{
-        type:DataTypes.STRING,
-        allowNull: false
-    },
-    cellPhone:{
-        type : DataTypes.DOUBLE,
-        allowNull : true
-    },
-    address:{
-        type:DataTypes.INTEGER,
-        allowNull: true
-    },
-    city:{
-        type:DataTypes.INTEGER,
-        allowNull: true
-    },
-    province:{
-        type:DataTypes.INTEGER,
-        allowNull: true
-    },
-    cp:{
-        type:DataTypes.INTEGER,
-        allowNull: true
-    },
-    rol:{
-        type:DataTypes.INTEGER,
-        allowNull: false
-    }},
-   {
-    timestamps: false,
-   
-    tableName: 'users'
-   })
+        {
+
+            timestamps: false,
+
+            tableName: 'users'
+        })
     return User
-    }
+}
