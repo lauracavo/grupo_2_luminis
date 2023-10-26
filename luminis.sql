8e81b087c83ae928cbc23148dc91550b09693ab5
carop2580 <perezcarolinasn@gmail.com>
2023-10-25 21:42:16 -0300

    actualizacion luminis.sql

--- luminis.sql
+++ luminis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-10-2023 a las 00:34:15
+-- Tiempo de generación: 26-10-2023 a las 02:40:40
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -44,6 +44,21 @@ CREATE TABLE `categories` (
   `name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `categories`
+--
+
+INSERT INTO `categories` (`idCategory`, `name`) VALUES
+(1, 'infantil'),
+(2, 'suspenso'),
+(3, 'drama'),
+(4, 'utiles_escolares'),
+(5, 'politica'),
+(6, 'terror'),
+(7, 'sagas'),
+(8, 'docentes'),
+(9, 'libros_escolares');
+
 -- --------------------------------------------------------
 
 --
@@ -51,25 +66,25 @@ CREATE TABLE `categories` (
 --
 
 CREATE TABLE `imagesproducts` (
-  `idImgProduct` int(11) NOT NULL,
+  `idImgProduct` int(10) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `imageUrl` varchar(150) NOT NULL,
-  `idProduct` int(11) NOT NULL
+  `idProduct` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Estructura de tabla para la tabla `orders`
+-- Volcado de datos para la tabla `imagesproducts`
 --
 
-CREATE TABLE `orders` (
-  `idOrder` int(11) NOT NULL,
-  `price` double NOT NULL,
-  `amount` int(11) NOT NULL,
-  `idProduct` int(11) NOT NULL,
-  `idCart` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `imagesproducts` (`idImgProduct`, `name`, `idProduct`) VALUES
+(5, 'sinImagen.png', 16),
+(6, 'product-1698089155957.jpg', 17),
+(7, 'product-1698089155959.jpg', 17),
+(8, 'product-1698175135990.jpg', 18),
+(9, 'product-1698187272871.png', 19),
+(10, 'product-1698187272980.jpg', 19),
+(11, 'product-1698190450069.png', 20),
+(12, 'product-1698190450145.jpg', 20),
+(13, 'product-1698190450087.jpg', 20);
 
 -- --------------------------------------------------------
 
@@ -90,6 +105,17 @@ CREATE TABLE `products` (
   `stock` int(11) NOT NULL,
   `idCategory` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `products`
+--
+
+INSERT INTO `products` (`idProduct`, `name`, `brand`, `editorial`, `author`, `detail`, `characteristic`, `purchasePrice`, `salePrice`, `stock`, `idCategory`) VALUES
+(16, 'El Mounstro de Colores ', '', 'Flamboyant', 'Anna Llenas', 'El monstruo de colores identifica cada emoción con un color de esta manera es muy fácil para los niños poder identificar las emociones de manera gráfi', ' 25cm x 25cm ,44 páginas, Tapa  dura, Letra minúscula', 1000, 4120, 12, 1),
+(17, 'El Mounstro de Colores ', '', 'Flamboyant', 'Anna Llenas', 'El monstruo de colores identifica cada emoción con un color de esta manera es muy fácil para los niños poder identificar las emociones de manera gráfi', ' 25cm x 25cm ,44 páginas, Tapa  dura, Letra minúscula', 1020, 4125, 12, 1),
+(18, 'Caperucita Roja', '', 'la estacion', 'Los hermanos Grimm (Jacob y Wilhelm)', 'Todo comienza con una abuelita medio enfermucha, y una canasta con una deliciosa torta y un frasco de miel. Caperucita debe llevársela a su abuela, pa', ' IMPRENTA MAYÚSCULA, tapa blanda, edad recomendada 6 años, 49 paginas, medidas:  13.00 x 21.00, ', 1800, 3330, 4, 1),
+(19, 'libro', '', 'hola mundo', 'hola mundo', 'dsgfdsghjghjhgkjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj', 'jjhjkhkjf', 520, 3000, 15, 8),
+(20, 'gyjhgjgkhgjh', '', 'jhjhkhkj', 'jkhjkhkjh', 'hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh', 'hgjhgh', 235, 4566, 5646, 3);
 
 -- --------------------------------------------------------
 
@@ -138,14 +164,6 @@ ALTER TABLE `imagesproducts`
   ADD KEY `idProduct` (`idProduct`);
 
 --
--- Indices de la tabla `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`idOrder`),
-  ADD KEY `idProduct` (`idProduct`),
-  ADD KEY `idCart` (`idCart`);
-
---
 -- Indices de la tabla `products`
 --
 ALTER TABLE `products`
@@ -172,19 +190,19 @@ ALTER TABLE `carts`
 -- AUTO_INCREMENT de la tabla `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `idCategory` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idCategory` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `imagesproducts`
 --
 ALTER TABLE `imagesproducts`
-  MODIFY `idImgProduct` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idImgProduct` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- AUTO_INCREMENT de la tabla `orders`
+-- AUTO_INCREMENT de la tabla `products`
 --
-ALTER TABLE `orders`
-  MODIFY `idOrder` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `products`
+  MODIFY `idProduct` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
@@ -206,14 +224,7 @@ ALTER TABLE `carts`
 -- Filtros para la tabla `imagesproducts`
 --
 ALTER TABLE `imagesproducts`
-  ADD CONSTRAINT `imagesproducts_ibfk_1` FOREIGN KEY (`idProduct`) REFERENCES `products` (`idProduct`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`idProduct`) REFERENCES `products` (`idProduct`),
-  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`idCart`) REFERENCES `carts` (`idCart`);
+  ADD CONSTRAINT `imagesproducts_ibfk_1` FOREIGN KEY (`idProduct`) REFERENCES `products` (`idProduct`);
 
 --
 -- Filtros para la tabla `products`
