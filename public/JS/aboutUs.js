a22099bbd7d3c184d05ea5c63379aa92af10eedb
TomasD1998 <tomidistefano@hotmail.com>
2023-11-18 14:47:11 -0300

    Mas avances en aboutUs

--- public/JS/aboutUs.js
+++ public/JS/aboutUs.js
@@ -1,58 +1,52 @@
-const photoOne = document.querySelector('#photoOne');
-const photoTwo = document.querySelector('#photoTwo');
-const photoThree = document.querySelector('#photoThree');
-const photoFour = document.querySelector('#photoFour');
-const photoFive = document.querySelector('#photoFive');
-const photoSix = document.querySelector('#photoSix');
+// Obtén referencias a todas las fotos y descripciones
+const photos = document.querySelectorAll('.photo');
+const descs = document.querySelectorAll('.description');
 
-const descOne = document.querySelector('#descOne');
-const descTwo = document.querySelector('#descTwo');
-const descThree = document.querySelector('#descThree');
-const descFour = document.querySelector('#descFour');
-const descFive = document.querySelector('#descFive');
-const descSix = document.querySelector('#descSix');
+// Función para mostrar el texto
+function showText(descElement) {
+  descElement.style.display = 'block';
+}
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Obtén todas las imágenes y descripciones
-    const photos = document.querySelectorAll('.photo');
-    const descriptions = document.querySelectorAll('.description');
-  
-    // Añade un event listener a cada imagen
-    photos.forEach(function (photo) {
-      photo.addEventListener('click', function () {
-        // Muestra la descripción correspondiente a esta imagen
-        const description = photo.nextElementSibling;
-        toggleDescription(description);
-      });
-    });
-  
-    // Añade un event listener al documento para cerrar las descripciones al hacer clic fuera de las imágenes
-    document.addEventListener('click', function (event) {
-      // Verifica si se hizo clic dentro de una descripción
-      const isDescriptionClick = Array.from(descriptions).some(function (description) {
-        return description.contains(event.target);
-      });
-  
-      // Oculta todas las descripciones si no se hizo clic dentro de una descripción
-      if (!isDescriptionClick) {
-        hideAllDescriptions();
-      }
-    });
-  
-    // Función para alternar la visibilidad de una descripción
-    function toggleDescription(description) {
-      // Oculta todas las descripciones
-      hideAllDescriptions();
-  
-      // Muestra u oculta la descripción según su estado actual
-      description.style.display = (description.style.display === 'block') ? 'none' : 'block';
-    }
-  
-    // Función para ocultar todas las descripciones
-    function hideAllDescriptions() {
-      descriptions.forEach(function (description) {
-        description.style.display = 'none';
-      });
-    }
+// Función para ocultar el texto
+function hideText(descElement) {
+  descElement.style.display = 'none';
+}
+
+photos.forEach((photo, index) => {
+  photo.addEventListener('click', function (event) {
+    // Elimina la clase 'active' de todas las fotos y descripciones
+    photos.forEach(p => p.classList.remove('active'));
+    descs.forEach(d => d.classList.remove('active'));
+
+    // Agrega la clase 'active' a la foto y descripción clicadas
+    photo.classList.add('active');
+    descs[index].classList.add('active');
   });
+});
+
+// Agrega un evento de clic a cada foto
+photos.forEach((photo, index) => {
+  photo.addEventListener('click', function (event) {
+    // Muestra el texto correspondiente a la foto clicada
+    showText(descs[index]);
+
+    // Agrega un evento de clic al documento para ocultar el texto al hacer clic fuera de la foto
+    function hideDescOnOutsideClick(e) {
+      // Comprueba si el clic no ocurrió en la foto ni en sus descendientes
+      if (!photo.contains(e.target)) {
+        // Oculta el texto
+        hideText(descs[index]);
+
+        
+
+        // Remueve el evento de clic del documento después de ocultar el texto
+        document.removeEventListener('click', hideDescOnOutsideClick);
+      }
+    }
+
+    // Agrega el evento de clic al documento
+    document.addEventListener('click', hideDescOnOutsideClick);
+  });
+});
+
 
