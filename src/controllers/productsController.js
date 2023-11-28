5d9757b32072fafb0feff1c5ccd3648acfbfc53d
carop2580 <perezcarolinasn@gmail.com>
2023-11-28 14:27:58 -0300

    product.ejs

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,18 +1,16 @@
-const path = require("path");
+
 const db = require('../../database/models/index')
 
 const productsController = {
     getAll: async (req, res) => {
-        // OBTENIENDO LOS DATOS DE LA BASE DE DATOS    
-        try {
-            let product = await db.Product.findAll()
-
-            for (let item of product) {
-
-                const imgList = await db.ImageProduct.findOne({ where: { idProduct: item.idProduct } });
-                product = [...product, { ...item.dataValues, imgList: imgList.dataValues }]
-                //     console.log(imgList.dataValues)
-            }
+               // OBTENIENDO LOS DATOS DE LA BASE DE DATOS    
+               try{
+                let products = await db.Product.findAll();
+                const product = await Promise.all(products.map(async (item) => {
+                       const imgList = await db.ImageProduct.findOne({ where: {idProduct: item.idProduct}});
+                       return { ...item.dataValues, imgList: imgList.dataValues };
+                        }));
+            
             let userLogged
             if (req.session.successLoginUser) {
                 userLogged = true;
