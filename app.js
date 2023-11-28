c1e6ea7036ac8487f17d692838233ca36b002f2b
TomasD1998 <tomidistefano@hotmail.com>
2023-11-28 12:53:53 -0300

    Modificacion en app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
+const cors = require ("cors");
 const app = express();
 
 //configuracion de cors
-const cors = require ("cors");
 app.use (cors());
 
 const session = require("express-session");
